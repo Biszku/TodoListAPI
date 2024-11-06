d587dbda6d7231dab02b87a50f6113f7783b9be9
Biszku <daniel.bivhuniak@gmail.com>
2024-11-06 19:58:26 +0100

    initial commit

---
+++ src/test/java/com/todolistapi/todolistapi/TodoListApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.todolistapi.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
